a8e8226d72df73f4169c446ded6a392d8d652fdb
zj-github <zj_github@163.com>
2016-01-07 20:25:49 +0800

    dd

---
+++ codelib/javase/src/main/java/a29_ds/list/MyArrayList.java
@@ -0,0 +1,61 @@
+package a29_ds.list;
+
+public class MyArrayList<T> {
+	private static final int DEFAULT_CAPACITY = 10;// DEFAULT_CAPACITY 容量 10
+	private T[] items;
+	private int listSize;
+
+	public MyArrayList() {
+		init(DEFAULT_CAPACITY);
+	}
+
+	public int size() {
+		return listSize;
+	}
+
+	public T get(int idx) {
+		return items[idx];
+	}
+
+	public void add(T t) {
+		
+		if(listSize==items.length){//超出了长度
+			//1、创建一个新数组
+			@SuppressWarnings("unchecked")
+			T[] newItems = (T [])new Object[listSize+DEFAULT_CAPACITY];
+			//2、把旧数组中的对象，移动到新数组
+			System.arraycopy(items, 0, newItems, 0, listSize);
+			items = newItems;
+			newItems =null;
+		}
+		System.out.println(listSize+"  "+ items.length);
+		items[listSize] = t;
+		listSize++;
+	}
+
+	/**
+	 * 初始化数组
+	 * @param initSize
+	 */
+	@SuppressWarnings("unchecked")
+	public void init(int initSize) {
+		if (initSize < listSize)
+			return;
+		items = (T[]) new Object[initSize]; // 创建一个新数组
+	}
+
+	public void remove(int idx) {
+		// 把对应角标后面的值，往前移一位
+		for (int i = idx; i < listSize - 1; i++) {
+			items[i] = items[i + 1];
+		}
+		listSize--;
+	}
+
+	public static void main(String[] args) {
+		MyArrayList<String> a = new MyArrayList<>();
+		for (int i = 0; i < 20; i++) {
+			a.add("a"+i);
+		}
+	}
+}
---
+++ codelib/javase/src/main/java/a29_ds/list/linklist/doubly/linked/list/DoublyLinkedList.java
@@ -0,0 +1,15 @@
+package a29_ds.list.linklist.doubly.linked.list;
+
+/**
+ * 双向链表：
+ * 每个节点不光保存了其前驱，还保存了其后驱
+ * @author oatt1
+ */
+public class DoublyLinkedList {
+
+//	public static void main(String[] args) {
+//		
+//	}
+	
+	
+}
