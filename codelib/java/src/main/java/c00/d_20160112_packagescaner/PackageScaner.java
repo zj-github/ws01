a1f6611030fad3a891240c0e36ae9c2572b5633b
zj-github <zj_github@163.com>
2016-01-13 15:41:51 +0800

    -A

--- codelib/java/src/main/java/c00/d_20160112_packagescaner/PackageScaner.java
+++ codelib/java/src/main/java/c00/d_20160112_packagescaner/PackageScaner.java
@@ -1,9 +1,9 @@
 package c00.d_20160112_packagescaner;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.TreeSet;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -11,7 +11,6 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 import org.apache.log4j.Logger;
-import org.junit.Test;
 
 /**
  * @ClassName: PackageScaner 
@@ -23,22 +22,40 @@ import org.junit.Test;
 /**
  * 
  * 1、工具类 JarFile 2、classpath的概念 3、ClassLoader获取ClassPath
- * 
  */
+// 1、获取当前线程的ClassLoader
+// 2、通过ClassLoader获取classpath
+// 3、遍历ClassPath获取jar包
+// 4、遍历jar包中的路径，取得指定的包名
+// 5、获取指定的包名下的class结尾的文件
 public class PackageScaner {
 	private Logger logger = Logger.getLogger(PackageScaner.class);
 
-	private final String JAR_SUFFIX = ".jar";
-	private final String CLASS_SUFFIX = ".jar";
-
-	public static void main(String[] args) {
+	public TreeSet<String> scan(String packageName) throws Exception {
+		System.out.println("------ start scan ---------");
+		PackageScaner pscaner = new PackageScaner();
 		// 1、获取当前线程的ClassLoader
+		URLClassLoader urlClassLoader = pscaner.getURLClassLoader();
 		// 2、通过ClassLoader获取classpath
-		// 3、遍历ClassPath获取jar包
-		// 4、遍历jar包中的路径，取得指定的包名
-		// 5、获取指定的包名下的class结尾的文件
+		URL[] classPathes = pscaner.getClassPath(urlClassLoader);
+		for (URL url : classPathes) {
 
+			System.out.println(url.toURI());
+		}
+		// 3、遍历ClassPath获取jar包
+		TreeSet<String> jarFile = pscaner.getJarFile(classPathes);
+		for (String string : jarFile) {
+			System.out.println(" >> jar file " + string);
+		}
+		// 4、遍历jar包中的路径，取得指定的包名下的class文件
+
+		System.out.println(" package name is " + packageName);
+		TreeSet<String> classes = pscaner.getSpePackageClass(jarFile, packageName);
+		return classes;
 	}
+
+	private final String JAR_SUFFIX = ".jar";
+	private final String CLASS_SUFFIX = ".class";
 
 	public URLClassLoader getURLClassLoader() {
 		return (URLClassLoader) Thread.currentThread().getContextClassLoader();
@@ -49,25 +66,12 @@ public class PackageScaner {
 		return urLs;
 	}
 
-	public void getJarFile(URL[] urls) throws Exception {
-		CopyOnWriteArrayList<JarFile> jarFileList = new CopyOnWriteArrayList<>();
+	public TreeSet<String> getJarFile(URL[] urls) throws Exception {
+		TreeSet<String> jarFiles = new TreeSet<>();
 		for (URL url : urls) {
-			String classPath = url.getPath();//
-			// 如果是jar文件
-			if (JAR_SUFFIX.endsWith(classPath)) {
-				jarFileList.add(new JarFile(classPath));
-			} else if (classPath.indexOf(".") != -1) {// 不是以点结尾，则封装成File对象，判断是否是文件夹
-				File file = new File(classPath);
-				boolean directory = file.isDirectory();
-				if (directory) {
-
-				} else {
-
-				}
-
-			}
-
+			jarFiles.addAll(getJarFilesFromDir(url.getPath()));
 		}
+		return jarFiles;
 	}
 
 	/**
@@ -79,49 +83,80 @@ public class PackageScaner {
 	 * @throws Exception
 	 *
 	 */
-	public void getJarFilesFromDir(File file, TreeSet<JarFile> jarFileList) throws Exception {
-		logger.debug(" scan dir path " + file.getAbsolutePath());
-		if (file.isDirectory()) {
-			File[] list = file.listFiles();
-			for (File f : list) {
-				String absolutePath = f.getAbsolutePath();
-				logger.debug("    * list path >> " + absolutePath);
-				getJarFilesFromDir(new File(absolutePath), jarFileList);
-			}
-		} else {
-			if (file.getName().indexOf(JAR_SUFFIX) != -1) {
-				logger.debug(" scan dir path " + file.getAbsolutePath());
-				if (jarFileList.add(new JarFile(file))) {
-					logger.info("add jar file " + file.toPath());
+	public void getJarFilesFromDir(File file, TreeSet<String> jarFileList) {
+		try {
+			logger.debug(" scan dir path " + file.getAbsolutePath());
+			if (file.isDirectory()) {
+				File[] list = file.listFiles();
+				for (File f : list) {
+					String absolutePath = f.getAbsolutePath();
+					System.out.println("    * list path >> " + absolutePath);
+					logger.debug("    * list path >> " + absolutePath);
+					getJarFilesFromDir(new File(absolutePath), jarFileList);
+				}
+			} else {
+				if (file.getName().indexOf(JAR_SUFFIX) != -1) {
+					logger.debug(" scan dir path " + file.getAbsolutePath());
+					if (jarFileList.add(file.getAbsolutePath())) {
+						logger.debug("add jar file " + file.toPath());
+					}
 				}
 			}
-		}
-	}
-
-	@Test
-	public void getJarFilesFromDirtest() {
-		TreeSet<JarFile> jarFileList = new TreeSet<>();
-		try {
-			getJarFilesFromDir(new File("E:\\devsf\\java\\jdk1.7.0_51"), jarFileList);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(Arrays.toString(jarFileList.toArray()));
 	}
 
-	public void getSpePackage(TreeSet<JarFile> jarFileList) {
+	/**
+	 * 
+	 * @Description: 获取指定目录下的所有的class
+	 * @author zhangjie
+	 * @date 2016年1月13日 下午2:48:22
+	 * @param dir
+	 * @param packageName
+	 * @return
+	 *
+	 */
+	public TreeSet<String> getJarFilesFromDir(String dir) {
+		TreeSet<String> jarFileList = new TreeSet<>();
+		getJarFilesFromDir(new File(dir), jarFileList);
+		return jarFileList;
 	}
 
-	public void getClassesFromJarFile(JarFile jarFile, String packageName,TreeSet<String> classList) {
-		Enumeration<JarEntry> entries = jarFile.entries();
-		while (entries.hasMoreElements()) {
-			JarEntry jarEntry = entries.nextElement();
-			String name = jarEntry.getName();
-			packageName = packageName.replace(".", File.pathSeparator);
-			if (!jarEntry.isDirectory() && name.startsWith(packageName) && name.endsWith(CLASS_SUFFIX)
-					&& name.indexOf("$") == -1) {
-				classList.add(name);
+	public TreeSet<String> getSpePackageClass(TreeSet<String> jarFileList, String packageName) {
+		TreeSet<String> classList = new TreeSet<>();
+		for (String jarFilePath : jarFileList) {
+
+			getClassesFromJarFile(jarFilePath, packageName, classList);
+		}
+		return classList;
+	}
+
+	public void getClassesFromJarFile(String jarFilePath, String packageName, TreeSet<String> classList) {
+		JarFile jarFile = null;
+		try {
+			jarFile = new JarFile(jarFilePath);
+
+			Enumeration<JarEntry> entries = jarFile.entries();
+			while (entries.hasMoreElements()) {
+				JarEntry jarEntry = entries.nextElement();
+				String name = jarEntry.getName();
+				packageName = packageName.replace(".", "/");
+				if (!jarEntry.isDirectory() && name.startsWith(packageName) && name.endsWith(CLASS_SUFFIX)
+						&& name.indexOf("$") == -1) {
+					classList.add(name.replace("/", ".").replace(".class", ""));
+					logger.info("-- " + name);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (jarFile != null) {
+				try {
+					jarFile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
--- codelib/java/src/main/java/c00_javase/a18_JarFile/JarFileTest.java
+++ codelib/java/src/main/java/c00_javase/a18_JarFile/JarFileTest.java
@@ -1,8 +1,10 @@
 package c00_javase.a18_JarFile;
 
+import java.io.File;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 import org.junit.Test;
 
@@ -33,6 +35,16 @@ public class JarFileTest {
 			}
 			
 		}
+		jarFile.close();
+	}
+	@Test
+	public void test2() throws Exception{
+		
+		JarFile jarFile = new JarFile("e:\\tools.jar");
+		
+		String name = "com/sun/tools/internal/xjc/reader/relaxng/";
+		ZipEntry entry = jarFile.getEntry(name);
+		System.out.println(entry.isDirectory());
 		jarFile.close();
 	}
 }
